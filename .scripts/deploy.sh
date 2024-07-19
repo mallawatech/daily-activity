23751dcb92ffc9ece68a09b68c29b3c144bcae6c
mallawatech <mallawaconnection@gmail.com>
2024-07-19 11:53:04 +0800

    update

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -7,24 +7,17 @@ echo "Deployment started ..."
 # if already is in maintenance mode
 (php artisan down) || true
 
-# Pull the latest version of the app
-git pull origin main
-
 # Install composer dependencies
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
-# Clear the old cache
-php artisan clear-compiled
-
-# Recreate cache
-php artisan optimize
-
-# Compile npm assets
-npm run prod
-
-# Run database migrations
-php artisan migrate --force
-
+git reset --hard HEAD
+git pull origin main --no-ff
+composer install --no-dev --prefer-dist --optimize-autoloader --no-interaction
+composer dump-autoload
+php artisan cache:clear
+php artisan config:cache
+php artisan config:clear
+php artisan route:cache
+php artisan view:cache
+php artisan storage:link
 # Exit maintenance mode
 php artisan up
 
