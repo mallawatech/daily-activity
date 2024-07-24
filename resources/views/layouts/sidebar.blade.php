ce3b8e4a971172c985c1fff6adb5c3fa300f04b7
mallawatech <mallawaconnection@gmail.com>
2024-07-25 02:09:59 +0800

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,7 +4,7 @@
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
         <div class="sidebar-brand-icon rotate-n-15">
-            <i class="fas fa-laugh-wink"></i>
+            <img src="{{ asset('img-logo/Logo.png') }}" alt="Logo" class="landing-image" width="50px" height="50px">
         </div>
         <div class="sidebar-brand-text mx-3">DaiRe-Apps</div>
     </a>
