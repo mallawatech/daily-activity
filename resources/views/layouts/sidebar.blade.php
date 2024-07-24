acfc89c2d83aef26ce2809dfdd29812a5767a535
mallawatech <mallawaconnection@gmail.com>
2024-07-25 02:06:21 +0800

    update

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -32,6 +32,25 @@
 </li>
 
 <!-- Divider -->
+<hr class="sidebar-divider">
+<div class="sidebar-heading">
+    Users
+</div>
+
+<!-- Nav Item - Pages Collapse Menu -->
+<li class="nav-item">
+    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+        aria-expanded="true" aria-controls="collapseTwo">
+        <i class="fas fa-fw fa-cog"></i>
+        <span>Users</span>
+    </a>
+    <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+            <h6 class="collapse-header">Data Users:</h6>
+            <a class="collapse-item" href="{{ route('admin.users') }}">Show All</a>
+        </div>
+    </div>
+</li>
 
     @elseif(Auth::user()->role == 'user')
               <!-- Heading -->
