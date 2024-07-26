83c94ea70ca29480d1a18283dd094402626619b1
mallawatech <mallawaconnection@gmail.com>
2024-07-26 16:39:03 +0800

    update

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -79,7 +79,6 @@
     <div class="card shadow mb-4">
         <div class="card-header py-3 d-flex justify-content-between align-items-center">
             <h6 class="m-0 font-weight-bold text-primary">All Reports</h6>
-            
         </div>
         <div class="card-body">
             {{-- PDF --}}
--- resources/views/admin/user.blade.php
+++ resources/views/admin/user.blade.php
@@ -1,47 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="card shadow mb-4">
+    <div class="card-header py-3 d-flex justify-content-between align-items-center">
+        <h5 class="m-0 font-weight-bold text-primary">User List</h5>
+        <div class="h5 mb-0 font-weight-bold text-primary">Total User : {{ \App\Models\User::count() }} User</div>
+    </div>
         <div class="card shadow mb-4">
-            <div class="card-header py-3 d-flex justify-content-between align-items-center">
-                <h6 class="m-0 font-weight-bold text-primary">User List</h6>
-                <div class="h5 mb-0 font-weight-bold text-primary">Total User : {{ \App\Models\User::count() }} User</div>
-            </div>
-            <div class="table-responsive">
-                <table class="table table-bordered">
-                    <thead>
-                        <tr>
-                            <th class="text-center">Name</th>
-                            <th class="text-center">Email</th>
-                            <th class="text-center">Satker</th>
-                            <th class="text-center">Kode EOS</th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        @foreach($users as $user)
-                        <tr>
-                            <td>{{ $user->name }}</td>
-                            <td>{{ $user->email }}</td>
-                            <td>{{ $user->satker }}</td>
-                            <td>{{ $user->kode_eos }}</td>
-                            <td class="text-center">
-                                <button type="button" class="btn btn-danger btn-sm" onclick="confirmDelete({{ $user->id }})">
-                                    <i class="fas fa-trash"></i> Delete
-                                </button>
-                                <form id="delete-form-{{ $user->id }}" action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display:none;">
-                                    @csrf
-                                    @method('DELETE')
-                                </form>
-                            </td>
-                        </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-                <div class="d-flex justify-content-center mt-4 mb-4">
-                    {{ $users->links() }} <!-- Link pagination -->
+            {{-- <div class="card-header py-3 d-flex justify-content-between align-items-center">
+                
+            </div> --}}
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Name</th>
+                                <th class="text-center">Email</th>
+                                <th class="text-center">Satker</th>
+                                <th class="text-center">Kode EOS</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($users as $user)
+                            <tr>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->satker }}</td>
+                                <td>{{ $user->kode_eos }}</td>
+                                <td class="text-center">
+                                    <button type="button" class="btn btn-danger btn-sm" onclick="confirmDelete({{ $user->id }})">
+                                        <i class="fas fa-trash"></i> Delete
+                                    </button>
+                                    <form id="delete-form-{{ $user->id }}" action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display:none;">
+                                        @csrf
+                                        @method('DELETE')
+                                    </form>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                    <div class="d-flex justify-content-center mt-4 mb-4">
+                        {{ $users->links() }} <!-- Link pagination -->
+                    </div>
                 </div>
             </div>
         </div>
+</div>
 
 
     <script>
